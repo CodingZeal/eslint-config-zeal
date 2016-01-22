0a7d26f9e780cc30e993772fe87a57654a1330c2
Randy Coulman <rcoulman@gmail.com>
2016-01-22 15:14:15 -0800

    Setup initial structure
    * Create package.json
    * Create ESLint skeleton files - index.js is the main configuration,
    with optional addons for Mocha, Chai, and React
    * Create .eslintrc.json for linting this project

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    node: true,
+    es6: true
+  },
+  ecmaFeatures: {
+    modules: true
+  },
+
+  globals: {},
+  rules: {}
+};
---
+++ react.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ecmaFeatures: {
+    jsx: true
+  },
+
+  rules: {}
+};
