e84f7eecc4861c368daa3d89587a3ea8c8daa6e6
Randy Coulman <rcoulman@gmail.com>
2016-01-26 12:13:04 -0800

    Add rules for variables

--- index.js
+++ index.js
@@ -195,6 +195,32 @@ module.exports = {
     // Strict Mode
     //
     // controls location of Use Strict Directives
-    'strict': [1, 'never']
+    'strict': [1, 'never'],
+
+    //
+    // Variables
+    //
+    // enforce or disallow variable initializations at definition
+    'init-declarations': 1,
+    // disallow the catch clause parameter name being the same as a variable in the outer scope
+    'no-catch-shadow': 1,
+    // disallow deletion of variables
+    'no-delete-var': 1,
+    // disallow labels that share a name with a variable
+    'no-label-var': 1,
+    // disallow shadowing of names such as arguments
+    'no-shadow-restricted-names': 1,
+    // disallow declaration of variables already declared in the outer scope
+    'no-shadow': 1,
+    // disallow use of undefined when initializing variables
+    'no-undef-init': 1,
+    // disallow use of undeclared variables unless mentioned in a /*global */ block
+    'no-undef': 1,
+    // disallow use of undefined variable
+    'no-undefined': 0,
+    // disallow declaration of variables that are not used in the code
+    'no-unused-vars': 1,
+    // disallow use of variables before they are defined
+    'no-use-before-define': [1, 'nofunc']
   }
 };
