1b589c6b8b986b1ac8cc7da2e1a90888cd24dbd2
Randy Coulman <rcoulman@gmail.com>
2017-01-09 10:14:39 -0800

    Add eslint-plugin-jest
    * Add eslint-plugin-jest and configure its two rules.
    
    * Jest 18.1 removes the `context` alias for `describe`, so we no longer
    need to specify `context` as a global.
    
    * Update README to talk about Jest

--- jest.js
+++ jest.js
@@ -2,15 +2,17 @@ module.exports = {
   env: {
     jest: true
   },
-
-  globals: {
-    context: true
-  },
-
+  plugins: [
+    'jest'
+  ],
   rules: {
     // specify the maximum depth callbacks can be nested
     'max-nested-callbacks': 'off',
     // specify the maximum number of statement allowed in a function
-    'max-statements': 'off'
+    'max-statements': 'off',
+    // disallow exclusive tests
+    'jest/no-exclusive-tests': 'warn',
+    // disallow identical titles
+    'jest/no-identical-title': 'warn'
   }
 }
