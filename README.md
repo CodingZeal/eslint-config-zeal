ff4211cde2be248118b3fc3479fb0830f22e61c7
Randy Coulman <rcoulman@gmail.com>
2018-10-12 11:46:55 -0700

    ⬆️ Upgrade to eslint-plugin-jest 21.24.1
    - New rule: `jest/expect-expect` defaults to `warn` with `td.verify` configured as an assertion method.
    - New rule: `jest/no-alias-methods` defaults to `off` because we prefer `toThrowError` over `toThrow` and there's no way to configure the rule to allow some aliases but not all.
    - New rule: `jest/no-test-return-statement` defaults to `warn`
    - New rule: `jest/prefer-inline-snapshots` defaults to `off`
    - New rule: `jest/prefer-strict-equal` defaults to `warn`
    - New rule: `jest/require-tothrow-message` defaults to `warn`

--- README.md
+++ README.md
@@ -135,7 +135,7 @@ This plugin contains all of the rules available in:
 - [eslint-plugin-react](https://github.com/yannickcr/eslint-plugin-react): 7.10.0
 - [eslint-plugin-react-native](https://github.com/intellicode/eslint-plugin-react-native): 3.2.1
 - [eslint-plugin-import](https://github.com/benmosher/eslint-plugin-import): 2.14.0
-- [eslint-plugin-jest](https://www.npmjs.com/package/eslint-plugin-jest): 21.15.0
+- [eslint-plugin-jest](https://www.npmjs.com/package/eslint-plugin-jest): 21.24.1
 
 ## License
 
--- jest.js
+++ jest.js
@@ -17,8 +17,16 @@ module.exports = {
     //
     // Enforce consistent test or it keyword
     "jest/consistent-test-it": ["warn", { fn: "test", withinDescribe: "test" }],
+    // Enforce assertion to be made in a test body
+    // Off for now, pending a resolution to https://github.com/jest-community/eslint-plugin-jest/issues/175
+    "jest/expect-expect": [
+      "warn",
+      { assertFunctionNames: ["expect", "td.verify"] }
+    ],
     // Disallow capitalized test names
     "jest/lowercase-name": "warn",
+    // Don't use alias methods
+    "jest/no-alias-methods": "off",
     // Disallow disabled tests
     "jest/no-disabled-tests": "warn",
     // Disallow focused tests
@@ -35,14 +43,22 @@ module.exports = {
     "jest/no-large-snapshots": "warn",
     // Disallow using f & x prefixes to define focused/skipped tests
     "jest/no-test-prefixes": "off",
+    // Disallow explicitly returning from tests
+    "jest/no-test-return-statement": "warn",
     // Suggest using expect.assertions() OR expect.hasAssertions()
     "jest/prefer-expect-assertions": "off",
+    // Suggest using inline snapshots
+    "jest/prefer-inline-snapshots": "off",
+    // Suggest using toStrictEqual()
+    "jest/prefer-strict-equal": "warn",
     // Suggest using toHaveLength()
     "jest/prefer-to-have-length": "warn",
     // Suggest using toBeNull()
     "jest/prefer-to-be-null": "warn",
     // Suggest using toBeUndefined()
     "jest/prefer-to-be-undefined": "warn",
+    // Require a message for toThrow()
+    "jest/require-tothrow-message": "warn",
     // Enforce valid describe() callback
     "jest/valid-describe": "warn",
     // Ensure expect is called correctly
